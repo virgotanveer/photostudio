1c631a5d310024fe2b39c829c31b5ef51c163d74
Tanveer Akram <tanveer.m122@gmail.com>
2025-07-23 11:03:49 +0000

    same

--- src/ai/flows/remove-background.ts
+++ src/ai/flows/remove-background.ts
@@ -14,7 +14,7 @@ const RemoveBackgroundInputSchema = z.object({
   photoDataUri: z
     .string()
     .describe(
-      "A photo, as a data URI that must include a MIME type and use Base64 encoding. Expected format: 'data:<mimetype>;base64,<encoded_data>'."
+      "A photo, as a data URI that must include a MIME type and use Base64 encoding. Expected format: 'data:<mimetype>;base64,<encoded_data>'"
     ),
 });
 export type RemoveBackgroundInput = z.infer<typeof RemoveBackgroundInputSchema>;
